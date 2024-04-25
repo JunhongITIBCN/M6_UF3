1cbd7cc9309ea604422a8a986acf9cedb2f298d7
JunhongITICBCN <2023_junhong.zhu@iticbcn.cat>
2024-04-25 16:53:03 +0200

    Canvis

--- buscador de pis/functions.js
+++ buscador de pis/functions.js
@@ -1,33 +1,114 @@
-  function validateNIF_NIE(value){
-    var validChars = 'TRWAGMYFPDXBNJZSQVHLCKET';
-    var nifRexp = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]{1}$/i;
-    var nieRexp = /^[XYZ]{1}[0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKET]{1}$/i;
-    var str = value.toString().toUpperCase();
+$(document).ready(function() {
+    $('#form-user-register').submit(function(e) {
+        e.preventDefault();
+        validateForm();
+    });
 
-    if (!nifRexp.test(str) && !nieRexp.test(str)) return false;
+    $('#validationNom, #validationCognoms, #validationDNI, #validationUsername, #validationEmail, #validationTelf').on('focusout', function() {
+        validateField($(this));
+    });
 
-    var nie = str
-      .replace(/^[X]/, '0')
-      .replace(/^[Y]/, '1')
-      .replace(/^[Z]/, '2');
+    $('#btnUsername').click(function() {
+        generateUsername();
+    });
 
-    var letter = str.substr(-1);
-    var charIndex = parseInt(nie.substr(0, 8)) % 23;
+    function validateForm() {
+        var isValid = true;
 
-    if (validChars.charAt(charIndex) === letter) return true;
+        isValid = validateField($('#validationNom')) && isValid;
+        isValid = validateField($('#validationCognoms')) && isValid;
+        isValid = validateField($('#validationDNI')) && isValid;
+        isValid = validateField($('#validationUsername')) && isValid;
+        isValid = validateField($('#validationEmail')) && isValid;
+        isValid = validateField($('#validationTelf')) && isValid;
 
-    return false;
-  }
+        if (isValid) {
+            $('#form-user-register').submit();
+        }
+    }
 
+    function validateField($field) {
+        var value = $field.val().trim();
+        var $feedback = $field.next('#feedback-' + $field.attr('id').substring(10));
 
-function validateEmail(mail) {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
-    alert("OK");
-  }else{
-    alert("KO");
-  }
-}
+        if (value === "") {
+            $field.addClass('is-invalid').removeClass('is-valid');
+            $feedback.html("Este campo es obligatorio.").addClass('invalid-feedback').removeClass('valid-feedback');
+            return false;
+        } else {
+            if ($field.attr('id') === 'validationDNI') {
+                if (!validateNIF_NIE(value)) {
+                    $field.addClass('is-invalid').removeClass('is-valid');
+                    $feedback.html("El DNI/NIE no es válido.").addClass('invalid-feedback').removeClass('valid-feedback');
+                    return false;
+                }
+            } else if ($field.attr('id') === 'validationEmail') {
+                if (!validateEmail(value)) {
+                    $field.addClass('is-invalid').removeClass('is-valid');
+                    $feedback.html("El correo electrónico no es válido.").addClass('invalid-feedback').removeClass('valid-feedback');
+                    return false;
+                }
+            } else if ($field.attr('id') === 'validationTelf') {
+                if (!validatePhone(value)) {
+                    $field.addClass('is-invalid').removeClass('is-valid');
+                    $feedback.html("El número de teléfono no es válido.").addClass('invalid-feedback').removeClass('valid-feedback');
+                    return false;
+                }
+            }
 
-$('#form-user-register').submit(function(e) {
-  e.preventDefault();
-  });
+            $field.removeClass('is-invalid').addClass('is-valid');
+            $feedback.html("").addClass('valid-feedback').removeClass('invalid-feedback');
+            return true;
+        }
+    }
+
+    function validateNIF_NIE(value) {
+        var validChars = 'TRWAGMYFPDXBNJZSQVHLCKET';
+        var nifRexp = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]{1}$/i;
+        var nieRexp = /^[XYZ]{1}[0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKET]{1}$/i;
+        var str = value.toString().toUpperCase();
+
+        if (!nifRexp.test(str) && !nieRexp.test(str)) return false;
+
+        var nie = str
+            .replace(/^[X]/, '0')
+            .replace(/^[Y]/, '1')
+            .replace(/^[Z]/, '2');
+
+        var letter = str.substr(-1);
+        var charIndex = parseInt(nie.substr(0, 8)) % 23;
+
+        return validChars.charAt(charIndex) === letter;
+    }
+
+    function validateEmail(mail) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail);
+    }
+
+    function validatePhone(phone) {
+        return /^[0-9]{9}$/.test(phone);
+    }
+
+    function generateUsername() {
+        var nom = $('#validationNom').val().trim().toLowerCase();
+        var cognoms = $('#validationCognoms').val().trim().toLowerCase().replace(/\s+/g, ''); 
+        var dni = $('#validationDNI').val().trim();
+
+        var primeraLletraNom = nom.charAt(0).toLowerCase();
+
+        var apellidos = cognoms.split(' ');
+        var primerCognom = apellidos[0];
+        var segundoCognom = apellidos[1] || '';
+
+        var numerosDNI = dni.match(/[02468]/g).join('');
+
+        var username = primeraLletraNom + primerCognom + segundoCognom + numerosDNI;
+
+        $('#validationUsername').val(username);
+    }
+    
+    $('#validationNom, #validationCognoms, #validationDNI, #validationUsername, #validationEmail, #validationTelf').on('focusin', function() {
+        $(this).removeClass('is-invalid');
+        $(this).next('#feedback-' + $(this).attr('id').substring(10)).html('').removeClass('invalid-feedback');
+    });
+});
--- buscador de pis/functions_pisos.js
+++ buscador de pis/functions_pisos.js
@@ -1,65 +1,95 @@
-select_districtes = document.getElementById("districtes")
-select_barris = document.getElementById("barris");
-select_barris.disabled = true;
+const selectDistrictes = document.getElementById("districtes");
+const selectBarris = document.getElementById("barris");
+const textPis = document.getElementById("nomPis");
+const textDir = document.getElementById("dir");
+const textPreu = document.getElementById("preu");
+const textText = document.getElementById("text");
+const nom_pis = document.getElementById("nom_pis");
+const vies = document.getElementById("vies");
+const nom_via = document.getElementById("nom_via");
+const numero_via = document.getElementById("numero_via");
+const pis = document.getElementById("pis");
+const escala = document.getElementById("escala");
+const porta = document.getElementById("porta");
+const cp = document.getElementById("cp");
+const preu_pis = document.getElementById("preu_pis");
+const textarea = document.getElementById("textarea");
 
-fetch("getDistrictes.php")
-.then((response) => response.json())
-.then((data) => {
-    data.forEach(value => {
-        let option = document.createElement("option"); 
-        option.value = value.name; 
-        option.innerHTML = value.name; 
-        select_districtes.appendChild(option); 
-    });
-})
-.catch((error) => {console.log("falla")});
+const showError = (element, message) => {
+    const feedback = document.getElementById(`feedback-${element.id}`);
+    element.classList.add('is-invalid');
+    feedback.textContent = message;
+};
 
-select_districtes.addEventListener("change", function() {
-    select_barris.disabled = false;
-    select_barris.innerHTML = "";
+const hideError = (element) => {
+    const feedback = document.getElementById(`feedback-${element.id}`);
+    element.classList.remove('is-invalid');
+    feedback.textContent = '';
+};
 
-    let formData = new FormData();
-    formData.append("id", select_districtes.selectedIndex);
-    let options = {
-        method: 'POST',
-        body: formData
+const validateInput = (element) => {
+    if (element.value.trim() === '') {
+        showError(element, 'Este campo es obligatorio.');
+        return false;
+    } else {
+        hideError(element);
+        return true;
     }
+};
 
-    fetch("getBarris.php", options)
-    .then((response) => response.json())
-    .then((data) => {
-        data.forEach(value => {
-            let option = document.createElement("option"); 
-            option.value = value.name; 
-            option.innerHTML = value.name; 
-            select_barris.appendChild(option); 
-        });
-    })
-})
+selectBarris.disabled = true;
+
+const fetchData = (url, options) => {
+    return fetch(url, options)
+        .then(response => response.json())
+        .catch(error => console.log("Error:", error));
+};
+
+const updateSelectOptions = (selectElement, data) => {
+    selectElement.innerHTML = "";
+    data.forEach(value => {
+        let option = new Option(value.name, value.name);
+        selectElement.appendChild(option);
+    });
+};
+
+const updateTextContent = () => {
+    textPis.textContent = `${nom_pis.value}: ${selectDistrictes.selectedOptions[0].text}, ${selectBarris.selectedOptions[0].text}`;
+    textDir.textContent = `${vies.options[vies.selectedIndex].text} ${nom_via.value} ${numero_via.value} ${pis.value} ${escala.value} ${porta.value} · ${cp.value} · ${selectDistrictes.selectedOptions[0].text} · ${selectBarris.selectedOptions[0].text}`;
+    textPreu.textContent = preu_pis.value;
+    textText.textContent = textarea.value;
+};
+
+fetchData("getDistrictes.php")
+    .then(data => updateSelectOptions(selectDistrictes, data));
+
+selectDistrictes.addEventListener("change", () => {
+    selectBarris.disabled = false;
+    fetchData("getBarris.php", {
+        method: 'POST',
+        body: new FormData().append("id", selectDistrictes.selectedIndex)
+    }).then(data => updateSelectOptions(selectBarris, data));
+});
 
 $('#form-user-register').submit(function(e) {
     e.preventDefault();
-    let textPis = document.getElementById("nomPis");
-    let textDir = document.getElementById("dir");
-    let textPreu = document.getElementById("preu");
-    let textText = document.getElementById("text");
+    const isValid = validateInput(nom_pis) && validateInput(vies) && validateInput(nom_via) && validateInput(numero_via) && validateInput(pis) && validateInput(escala) && validateInput(porta) && validateInput(cp) && validateInput(preu_pis) && validateInput(textarea);
+    if (isValid) {
+        updateTextContent();
+    }
+});
 
-    let nom_pis = document.getElementById("nom_pis");
-    let districtes = document.getElementById("districtes");
-    let barris = document.getElementById("barris");
-    let via = document.getElementById("vies");
-    let nom_via = document.getElementById("nom_via");
-    let numero_via = document.getElementById("numero_via");
-    let pis = document.getElementById("pis");
-    let escala = document.getElementById("escala");
-    let porta = document.getElementById("porta");
-    let cp = document.getElementById("cp");
-    let preu = document.getElementById("preu_pis");
-    let textarea = document.getElementById("textarea");
+nom_pis.addEventListener('focusout', () => validateInput(nom_pis));
+vies.addEventListener('focusout', () => validateInput(vies));
+nom_via.addEventListener('focusout', () => validateInput(nom_via));
+numero_via.addEventListener('focusout', () => validateInput(numero_via));
+pis.addEventListener('focusout', () => validateInput(pis));
+escala.addEventListener('focusout', () => validateInput(escala));
+porta.addEventListener('focusout', () => validateInput(porta));
+cp.addEventListener('focusout', () => validateInput(cp));
+preu_pis.addEventListener('focusout', () => validateInput(preu_pis));
+textarea.addEventListener('focusout', () => validateInput(textarea));
 
-  
-    textPis.innerHTML = nom_pis.value + ": " + districtes.options[districtes.selectedIndex].text + ", " + barris.options[barris.selectedIndex].text;
-    textDir.innerHTML = `${via.options[via.selectedIndex].text} ${nom_via.value} ${numero_via.value} ${pis.value} ${escala.value} ${porta.value} · ${cp.value} · ${districtes.options[districtes.selectedIndex].text} · ${barris.options[barris.selectedIndex].text}`;
-    textPreu.innerHTML = preu.value;
-    textText.innerHTML = textarea.value;
+[nom_pis, vies, nom_via, numero_via, pis, escala, porta, cp, preu_pis, textarea].forEach(element => {
+    element.addEventListener('focusin', () => hideError(element));
 });
